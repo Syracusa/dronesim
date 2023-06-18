4423c4c37a4cce948473dddb14bfb7d15646c512
Kijin Kim <msrndsy@gmail.com>
2023-06-18 11:42:25 +0900

    Babylonjs test code

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,31 +1,40 @@
-/**
- * This file will automatically be loaded by webpack and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/latest/tutorial/process-model
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
 
 import './index.css';
+import * as BABYLON from "babylonjs"
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+const canvas = document.getElementById("renderCanvas"); // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+const createScene = function () {
+    // Creates a basic Babylon Scene object
+    const scene = new BABYLON.Scene(engine);
+    // Creates and positions a free camera
+    const camera = new BABYLON.FreeCamera("camera1", 
+        new BABYLON.Vector3(0, 5, -10), scene);
+    // Targets the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+    // Creates a light, aiming 0,1,0 - to the sky
+    const light = new BABYLON.HemisphericLight("light", 
+        new BABYLON.Vector3(0, 1, 0), scene);
+    // Dim the light a small amount - 0 to 1
+    light.intensity = 0.7;
+    // Built-in 'sphere' shape.
+    const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", 
+        {diameter: 2, segments: 32}, scene);
+    // Move the sphere upward 1/2 its height
+    sphere.position.y = 1;
+    // Built-in 'ground' shape.
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", 
+        {width: 6, height: 6}, scene);
+    return scene;
+};
+const scene = createScene(); //Call the createScene function
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function () {
+        scene.render();
+});
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+        engine.resize();
+});
