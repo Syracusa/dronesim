65ab64ea6f487ec57394af77fd352c548b86866c
Syracusa <msrndsy@gmail.com>
2023-06-24 08:39:22 +0900

    Update

--- src/scene/ShiftHelper.ts
+++ src/scene/ShiftHelper.ts
@@ -30,15 +30,44 @@ export class ShiftHelper {
         const arrow = BABYLON.Mesh.MergeMeshes([arrowTip, arrowBody], true, true, undefined, false, true);
 
         const mat = new BABYLON.StandardMaterial("arrowMat", scene);
-        mat.diffuseColor = new BABYLON.Color3(0.5, 1.0, 0.0);
-        mat.specularColor = new BABYLON.Color3(0.5, 0.5, 0.5);
-        mat.emissiveColor = new BABYLON.Color3(0.5, 1.0, 0.0);
-        mat.alpha = 9.9;
+        mat.diffuseColor = new BABYLON.Color3(0.0, 1.0, 0.0);
+        mat.specularColor = new BABYLON.Color3(0.0, 0.0, 0.0);
+        mat.emissiveColor = new BABYLON.Color3(0.0, 0.7, 0.0);
+
         mat.backFaceCulling = false;
 
         arrow.metadata = "arrow";
         arrow.material = mat;
         arrow.position = new BABYLON.Vector3(40, 5, 40);
+
+
+        const matRed = new BABYLON.StandardMaterial("arrowMat", scene);
+        matRed.diffuseColor = new BABYLON.Color3(1.0, 0.0, 0.0);
+        matRed.specularColor = new BABYLON.Color3(0.0, 0.0, 0.0);
+        matRed.emissiveColor = new BABYLON.Color3(0.7, 0.0, 0.0);
+
+        matRed.backFaceCulling = false;
+
+
+        const matBlue = new BABYLON.StandardMaterial("arrowMat", scene);
+        matBlue.diffuseColor = new BABYLON.Color3(0.0, 0.0, 1.0);
+        matBlue.specularColor = new BABYLON.Color3(0.0, 0.0, 0.0);
+        matBlue.emissiveColor = new BABYLON.Color3(0.0, 0.0, 0.7);
+
+        matBlue.backFaceCulling = false;
+
+
+        let arrowX = arrow;
+        arrowX.rotation.x = Math.PI / 2;
+
+        let arrowY = arrow.clone("arrowY");
+        arrowY.rotation.y += Math.PI / 2;
+        arrowY.material = matRed;
+        
+        let arrowZ = arrow.clone("arrowZ");
+        arrowZ.rotation.z += Math.PI / 2;
+        arrowZ.material = matBlue;
+
 
         this.arrow = arrow;
     }
