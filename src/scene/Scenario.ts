a8539db0d1debb0a9b89c5a11aac456fd0778df6
Kijin Kim <msrndsy@gmail.com>
2023-07-08 15:50:01 +0900

    Scenario class

---
+++ src/scene/Scenario.ts
@@ -0,0 +1,13 @@
+import * as BABYLON from "@babylonjs/core/Legacy/legacy";
+export class Scenario {
+    nodeNum = 30;
+    nodeCurrPos: BABYLON.Vector3[] = [];
+
+    constructor() {
+    
+    }
+
+    update(timeDiff: number) {
+
+    }
+}
